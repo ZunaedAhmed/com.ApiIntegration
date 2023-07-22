15004a1f340e4c44344bc7a5ef84da8754a9b63d
ZunaedAhmed <zunaed.ahmed2@gmail.com>
2023-07-22 16:38:21 +0900

    Initial commit

---
+++ src/main/java/ApiIntegration/com/ApiIntegration/Application.java
@@ -0,0 +1,13 @@
+package ApiIntegration.com.ApiIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/ApiIntegration/com/ApiIntegration/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ApiIntegration.com.ApiIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
